045ca4174576373d3d91d12ed3440ef47a7a3a30
Yennifer Ortega <yenniferso.ortegaoc@ecci.edu.co>
2022-05-23 02:49:37 -0500

    n
    n

--- src/main/java/com/mycompany/discretas_proyecto/Components.java
+++ src/main/java/com/mycompany/discretas_proyecto/Components.java
@@ -1,17 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.discretas_proyecto;
 
-import java.awt.Color;
 import javax.swing.JFrame;
 
 /**
  *
  * @author Asus
  */
-public class Discretas_Proyecto {
+public class Components {
+    
     
 }
